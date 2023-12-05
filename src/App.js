be882309fd3f36a5d12989fb8dad4cb5494ed4f9
Salem <barhmisalem44.iss@gmail.com>
2023-12-05 18:19:30 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css'; // Import your CSS styles if any
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
